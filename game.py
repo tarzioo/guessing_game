6b0796732ba7ff6741efee7106bc879c603cc7da
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 18:04:39 -0700

    Added game repetition feature and added detection of numbers not in range as well as characters

--- game.py
+++ game.py
@@ -1,19 +1,30 @@
 # Put your code here
 import random
-rand_num = random.randint(1,100)
+rand_num = random.randint(1,2)
 
-guesses = 1
-user = raw_input("Hey, What's your name? ")
-print "Hey %s, let's play a game! Try to guess what number I'm thinking of" % user
-num = int(raw_input("Hint, it's between 1 and 100: "))
 
-while num != rand_num:
-    if num > rand_num:
-        print "Too High, guess again"    
-    else:
-        print "Too Low, guess again"
+play = 'Y'
+while play.upper() == 'Y':
+    guesses = 1
+    user = raw_input("Hey, What's your name? ")
+    print "Hey %s, let's play a game! Try to guess what number I'm thinking of" % user
+    num = int(raw_input("Hint, it's between 1 and 100: "))
+    try:
+        while num != rand_num:
+            if num > rand_num and num < 101:
+                print "Too High, guess again"    
+            elif num < rand_num and num > 0:
+                print "Too Low, guess again"
+            else:
+                print "Silly goose, read the directions and guess between 1 and 100"
+            
 
-    guesses += 1
-    num = int(raw_input("Guess again a number between 1 and 100: "))
-        
-print "Great job, you guessed it in %d guesses" % guesses
+            guesses += 1
+            num = int(raw_input("Guess again a number between 1 and 100: "))
+                
+        print "Great job, you guessed it in %d guesses" % guesses
+    except ValueError:
+        print "Silly goose, I asked for a number, guess a number between 1 and 100"
+    play = raw_input("Would you like to play again (Y/N): ")
+
+print "Thanks for playing!"    
